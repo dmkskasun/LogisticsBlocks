4fcb3fcf34d3a19c1ca0e30347eedd89e9745c03
bhathiyaamarasinghe <bhathiyaamare98@gmail.com>
2021-06-04 02:31:29 +0530

    added css

---
+++ logistics-blobks/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                
+      <footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <p><i class="fa fa-copyright"></i> Copyright 2020 by Logistics Blocks
+
+           </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+          
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ logistics-blobks/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <header class="main-header clearfix" role="header">
+                    <div class="logo">
+                        <a href="#"><em>Logistics</em>Blocks</a>
+                    </div>
+                    <a href="#menu" class="menu-link"><i class="fa fa-bars"></i></a>
+                    <nav id="menu" class="main-nav" role="navigation">
+                        <ul class="main-menu">
+                            <li><a href="#section1">Home</a></li>
+                            <li class="has-submenu"><a href="#section2">About Us</a>
+                                <ul class="sub-menu">
+                                    <li><a href="#section2">Who we are?</a></li>
+                                    <li><a href="#section3">What we do?</a></li>
+                                    <li><a href="#section3">How it works?</a></li>
+                                    <li><a href="https://templatemo.com/about" rel="sponsored" class="external">External URL</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#section4">Services</a></li>
+                            {/* <!-- <li><a href="#section5">Video</a></li>  */}
+                            <li><a href="#section6">Contact</a></li>
+                            <li><a href="" class="external">Careers</a></li>
+                        </ul>
+                    </nav>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Header;
